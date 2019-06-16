5acb094cdc5ae140929a322c135d202d47129933
harishpython1 <harish.python.com@gmail.com>
2019-06-16 15:42:54 +0530

    Mouse hover scenario added

---
+++ MouseHoverDemo.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+driver=webdriver.Chrome()
+
+driver.get("https://www.amazon.in/")
+driver.implicitly_wait(30)
+a1=ActionChains(driver)
+category_ele = driver.find_element_by_xpath("//span[text()='Category']")
+a1.move_to_element(category_ele).perform()
+
